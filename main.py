befaaf63690b13504fb1f590bad119a5a9917f35
Yakovlev <yurpom22@yandex.ru>
2024-06-04 11:21:49 +0700

    add_var17

--- main.py
+++ main.py
@@ -1,31 +1,16 @@
-import csv
-import streamlit as st
+# This is a sample Python script.
 
-# from var04 import do_var04
-# from var06 import do_var06
-# from var11 import do_var11
-from var13 import do_var13
-# from var14 import do_var14
-# from var17 import do_var17
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-st.image('titanic1912.jpg')
-choice = st.selectbox ('Номер варианта:', ['Вариант №04', 'Вариант №06', 'Вариант №11', 'Вариант №13', 'Вариант №14', 'Вариант №17'])
 
-if choice == 'Вариант №04':
-    #do_var04()
-    st.write ('К сожалению данная страница находится в разработке. Но скоро вы сможете узнать процент выживших молодых (до 30) и старых (после 60) пассажиров, указав класс билета. Приносим свои извинения.')
-elif choice == 'Вариант №06':
-    # do_var06()
-    st.write ('К сожалению данная страница находится в разработке. Но скоро вы сможете узнать среднюю стоимость билета у спасенных и погибших, выбрав из списка порт посадки. Приносим свои извинения.')
-elif choice == 'Вариант №11':
-    # do_var11()
-    st.write ('К сожалению данная страница находится в разработке. Но скоро вы сможете узнать долю погибших и спасенных пассажиров, указав возрастную категорию из списка - «молодой» (до 30 лет), «среднего возраста» (от 30 до 60) и «старый» (более 60 лет). Приносим свои извинения.')
-elif choice == 'Вариант №13':
-    do_var13()
-   # st.write ('К сожалению данная страница находится в разработке. Но скоро вы сможете узнать количество погибших детей по каждому пункту посадки, указав максимальный возраст (число от 1 до 18). Приносим свои извинения.')
-elif choice == 'Вариант №14':
-    # do_var14()
-    st.write ('К сожалению данная страница находится в разработке. Но скоро вы сможете узнать число родственников отдельно для выживших и погибших пассажиров указанного пола. Приносим свои извинения.')
-elif choice == 'Вариант №17':
-    # do_var17()
-    st.write ('К сожалению данная страница находится в разработке. Но скоро вы сможете узнать среднюю, минимальную или максимальную стоимость билета у пассажиров по каждому пункту посадки. Приносим свои извинения.')
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ var17.py
@@ -0,0 +1,79 @@
+# import streamlit as st
+
+def  get_bilet(lines):
+
+
+    S_bilet=0
+    S_passajir=0
+    S_min = -1
+    S_max = 0
+
+    C_bilet = 0
+    C_passajir = 0
+    C_min = -1
+    C_max = 0
+
+    Q_bilet = 0
+    Q_passajir = 0
+    Q_min = -1
+    Q_max = 0
+
+    for line in lines:
+        data = line.split(',')
+
+        if data[0] == 'PassengerId':
+            continue    #переход на следую строку
+        if data[10] == '' or data[10] == ' ':
+            continue  # Пропускаем пассажира без стоимости билета
+
+        if data[12].strip() == 'S':     #strip убирает переход - enter
+            S_passajir = S_passajir + 1
+            S_bilet = S_bilet + float(data[10])
+
+            if (S_min > float(data[10]) or S_min == -1) and float(data[10]) != 0:
+                S_min = float(data[10])
+            if S_max < float(data[10]):
+                S_max = float(data[10])
+
+        if data[12].strip() == 'C':     #strip убирает переход - enter
+            C_passajir = C_passajir + 1
+            C_bilet = C_bilet + float(data[10])
+
+            if (C_min > float(data[10]) or C_min == -1) and float(data[10]) != 0:
+                C_min = float(data[10])
+            if C_max < float(data[10]):
+                C_max = float(data[10])
+
+        if data[12].strip() == 'Q':     #strip убирает переход - enter
+            Q_passajir = Q_passajir + 1
+            Q_bilet = Q_bilet + float(data[10])
+
+            if (Q_min > float(data[10]) or Q_min == -1) and float(data[10]) != 0:
+                Q_min = float(data[10])
+            if Q_max < float(data[10]):
+                Q_max = float(data[10])
+
+
+    return round(S_bilet / S_passajir,2), S_min, S_max, round(C_bilet / C_passajir,2), C_min, C_max, round(Q_bilet / Q_passajir,2), Q_min, Q_max
+
+
+with open("data.csv") as file:
+   lines = file.readlines()
+   S_srednee, S_min, S_max, C_srednee, C_min, C_max, Q_srednee, Q_min, Q_max = get_bilet(lines)
+
+
+print("Средняя стоимость билета в порту посадки S:",  S_srednee)
+print("Минимальная стоимость билета в порту посадки S:",  round(S_min,2))
+print("Максимальная стоимость билета в порту посадки S:",  round(S_max,2))
+
+print()
+
+print("Средняя стоимость билета в порту посадки C:",  C_srednee)
+print("Минимальная стоимость билета в порту посадки C:",  round(C_min,2))
+print("Максимальная стоимость билета в порту посадки C:",  round(C_max,2))
+
+print()
+
+print("Средняя стоимость билета в порту посадки Q:",  Q_srednee)
+print("Минимальная стоимость билета в порту посадки Q:",  round(Q_min,2))
+print("Максимальная стоимость билета в порту посадки Q:",  round(Q_max,2))
