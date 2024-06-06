5e711405c1fc1e76835c4cb0518cf82b22ad23dc
Yakovlev <yurpom22@yandex.ru>
2024-06-06 12:12:11 +0700

    add_var17_ver 5.0+test-jpg

--- var17.py
+++ var17.py
@@ -1,95 +1,97 @@
 import streamlit as st
 import matplotlib.pyplot as plt
-
-
-#изменения от 06.06.
 def  get_bilet(lines):
-    with (open('data.csv') as file):
-        #вводим переменные с пассажирами и билетами
-        S_bilet = 0
-        S_passenger = 0
-        S_min = -1
-        S_max = 0
+    S_bilet=0
+    S_passajir=0
+    S_min = -1
+    S_max = 0
 
-        C_bilet = 0
-        C_passenger = 0
-        C_min = -1
-        C_max = 0
+    C_bilet = 0
+    C_passajir = 0
+    C_min = -1
+    C_max = 0
 
-        Q_bilet = 0
-        Q_passenger = 0
-        Q_min = -1
-        Q_max = 0
+    Q_bilet = 0
+    Q_passajir = 0
+    Q_min = -1
+    Q_max = 0
 
-        min_valueS = 0
+    for line in lines:
+        data = line.split(',')
 
-    #разделяем строку на значения и категоию
-        for line in file:
-            data = line.split(',')
+        if data[0] == 'PassengerId':
+            continue    #переход на следую строку
+        if data[10] == '' or data[10] == ' ':
+            continue  # Пропускаем пассажира без стоимости билета
 
-            #исключаем ID пассажира
-            if data[0] == 'PassengerId':
-                continue
+        if data[12].strip() == 'S':     #strip убирает переход - enter
+            S_passajir = S_passajir + 1
+            S_bilet = S_bilet + float(data[10])
 
-            #высчитываем количество пассажиров с портом пасадки S и общую сумму стоимости билетов
-            if data[12].strip() == 'S':
-                S_passenger = S_passenger + 1
-                S_bilet = S_bilet + float(data[10])
+            if (S_min > float(data[10]) or S_min == -1) and float(data[10]) != 0:
+                S_min = float(data[10])
+            if S_max < float(data[10]):
+                S_max = float(data[10])
 
-                if (S_min > float(data[10]) or S_min == -1) and float(data[10]) != 0:
-                    S_min = float(data[10])
-                if S_max < float(data[10]):
-                    S_max = float(data[10])
+        if data[12].strip() == 'C':     #strip убирает переход - enter
+            C_passajir = C_passajir + 1
+            C_bilet = C_bilet + float(data[10])
 
-            #высчитываем количество пассажиров с портом пасадки С и общую сумму стоимости билетов
-            if data[12].strip() == 'C':
-                C_passenger = C_passenger + 1
-                C_bilet = C_bilet + float(data[10])
+            if (C_min > float(data[10]) or C_min == -1) and float(data[10]) != 0:
+                C_min = float(data[10])
+            if C_max < float(data[10]):
+                C_max = float(data[10])
 
-                if (C_min > float(data[10]) or C_min == -1) and float(data[10]) != 0:
-                    C_min = float(data[10])
-                if C_max < float(data[10]):
-                    C_max = float(data[10])
+        if data[12].strip() == 'Q':     #strip убирает переход - enter
+            Q_passajir = Q_passajir + 1
+            Q_bilet = Q_bilet + float(data[10])
 
-            # высчитываем количество пассажиров с портом пасадки Q и общую сумму стоимости билетов
-            if data[12].strip() == 'Q':
-                Q_passenger = Q_passenger + 1
-                Q_bilet = Q_bilet + float(data[10])
-
-                if (Q_min > float(data[10]) or Q_min == -1) and float(data[10]) != 0:
-                    Q_min = float(data[10])
-                if Q_max < float(data[10]):
-                    Q_max = float(data[10])
+            if (Q_min > float(data[10]) or Q_min == -1) and float(data[10]) != 0:
+                Q_min = float(data[10])
+            if Q_max < float(data[10]):
+                Q_max = float(data[10])
 
 
+    return round(S_bilet / S_passajir,2), S_min, S_max, round(C_bilet / C_passajir,2), C_min, C_max, round(Q_bilet / Q_passajir,2), Q_min, Q_max
 
-    #вводим переменные средней, минимальной и максимальной стоимости по портам посадки и присваиваем им значение из вычеслений выше
-    averageS = S_bilet / S_passenger
-    averageC = C_bilet / C_passenger
-    averageQ = Q_bilet / Q_passenger
-    minimumS = round(S_min,2)
-    minimumC = round(C_min,2)
-    minimumQ = round(Q_min,2)
-    maximumS = round(S_max,2)
-    maximumC = round(C_max,2)
-    maximumQ = round(Q_max,2)
+
+with open("data.csv") as file:
+   lines = file.readlines()
+   S_srednee, S_min, S_max, C_srednee, C_min, C_max, Q_srednee, Q_min, Q_max = get_bilet(lines)
+
+
+#print("Средняя стоимость билета в порту посадки S:",  S_srednee)
+#print("Минимальная стоимость билета в порту посадки S:",  round(S_min,2))
+#print("Максимальная стоимость билета в порту посадки S:",  round(S_max,2))
+
+#print()
+
+#print("Средняя стоимость билета в порту посадки C:",  C_srednee)
+#print("Минимальная стоимость билета в порту посадки C:",  round(C_min,2))
+#print("Максимальная стоимость билета в порту посадки C:",  round(C_max,2))
+
+#print()
+
+#print("Средняя стоимость билета в порту посадки Q:",  Q_srednee)
+#print("Минимальная стоимость билета в порту посадки Q:",  round(Q_min,2))
+#print("Максимальная стоимость билета в порту посадки Q:",  round(Q_max,2))
+
+
 
 def do_var17():
-    st.image('Titanic.jpg')
-    st.header('Практическая работа Яковлева А.А.')
-    st.write('Вычисление средней, минимальной и максимальной стоимости билета у пассажиров по каждому пункту посадки.')
+    st.title ('Вычисление стоимости билета у пассажиров по каждому пункту посадки.')
     option = st.selectbox('Выбирите категорию стоимости билета', ['Средняя', 'Минимальная', 'Максимальная'])
     pport = ['Порт S', 'Порт C', 'Порт Q']
 
-    # если выбрана категория средней стоимости то переменная avg_port равна соответствующим переменным по портам посадки
+    #если выбрана категория средней стоимости то переменная avg_port равна соответствующим переменным по портам посадки
     if option == "Средняя":
-        avg_port = [averageS, averageC, averageQ]
+        avg_port = [S_srednee, C_srednee, Q_srednee]
 
     if option == "Минимальная":
-        avg_port = [minimumS, minimumC, minimumQ]
+        avg_port = [S_min, C_min, Q_min]
 
     if option == "Максимальная":
-        avg_port = [maximumS, maximumC, maximumQ]
+        avg_port = [S_max, C_max, Q_max]
 
     data = {'Порт посадки': pport, 'Cтоимость билета': avg_port}
     st.table(data)
